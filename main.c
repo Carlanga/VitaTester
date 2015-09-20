284549a33b20720f83a7ec1a8ce7943abe82a538
SMOKE <metallica5875@yahoo.com>
2015-09-20 01:39:17 -0400

    Revert "Some reorganizing"
    This reverts commit 3c63eba8c57906bffaf0da54406c2609c795e2e2.

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <psp2/ctrl.h>
+#include <psp2/kernel/processmgr.h>
+#include <psp2/moduleinfo.h>
+#include <vita2d.h>
+#include "font.h"
+#include "revitalize.h"
 
 PSP2_MODULE_INFO(0, 0, "VitaButtonTester");
+
+#define SCREEN_W 960
+#define SCREEN_H 544
+
+#define EXIT_COMBO (PSP2_CTRL_START | PSP2_CTRL_SELECT)
+
+#define ANALOG_THRESHOLD 100
+
+#define BLACK   RGBA8(  0,   0,   0, 255)
+#define WHITE   RGBA8(255, 255, 255, 255)
+#define GREEN   RGBA8(  0, 255,   0, 255)
+#define RED     RGBA8(255,   0,   0, 255)
+#define BLUE    RGBA8(  0,   0, 255, 255)
 
 void show_splash()
 {
